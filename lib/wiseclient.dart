a53437bc8e7ddaab8cee45b6e1f79d8b27a8cd34
Yani Maassen <yani.maassen@appwise.be>
2024-02-29 14:12:53 +0100

    Exported OAuthToken

--- lib/wiseclient.dart
+++ lib/wiseclient.dart
@@ -2,10 +2,11 @@
 library wiseclient;
 
 export 'package:dio/dio.dart' show DioException;
-export 'package:fresh_dio/fresh_dio.dart' show AuthenticationStatus, OAuth2Token, RevokeTokenException;
+export 'package:fresh_dio/fresh_dio.dart' show AuthenticationStatus, RevokeTokenException;
 
 export 'src/error_screens/error_screens.dart';
 export 'src/exceptions/exceptions.dart';
 export 'src/interceptors/interceptors.dart';
 export 'src/options.dart';
+export 'src/token_model/oauth_token.dart';
 export 'src/wiseclient_base.dart';
